669ccec16f945d1b0bd09ab5129c366ada772433
Daiki Kawanuma <KAWANUMA@jp.ibm.com>
2018-12-29 00:08:04 +0900

    Initial Commit

---
+++ TutorialSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialSample
+//
+//  Created by aa367474 on 2018/12/29.
+//  Copyright © 2018年 aa367474. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
